07370e27a0e1c0d6c0f36d5f993a910099180a82
Muhammed Nazir <mnazir9876@gmail.com>
2019-12-19 02:21:46 -0600

    delete for portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -45,4 +45,15 @@ class PortfoliosController < ApplicationController
         end
       end
 
+      def destroy
+        #[Performs the lookup]
+        @portfolio = Portfolio.find(params[:id])
+
+        @portfolio.destroy
+        respond_to do |format|
+          format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+      end
+
 end
