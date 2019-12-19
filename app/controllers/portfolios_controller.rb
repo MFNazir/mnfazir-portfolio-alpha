85bfb09ca70e54676472166a85835b226d84229b
mfnazir <mnazir9876@gmail.com>
2019-12-18 20:53:36 -0600

    new form for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,21 @@ class PortfoliosController < ApplicationController
         @portfolios = Portfolio.all     
     end
 
+    def new 
+        @portfolio = Portfolio.new
+    end 
+
+    def create
+        @portfolio = Portfolio.new(blog_params)
+    
+        respond_to do |format|
+          if @blog.save
+            format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+            format.json { render :show, status: :created, location: @blog }
+          else
+            format.html { render :new }
+            format.json { render json: @blog.errors, status: :unprocessable_entity }
+          end
+        end
+      end
 end
