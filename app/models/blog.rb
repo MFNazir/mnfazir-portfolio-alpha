3e4b4fa340887a3f34870f7ea21fc376abf36d2e
Muhammed Nazir <mnazir9876@gmail.com>
2019-12-20 19:24:00 -0600

    implemented validation for blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,4 +5,5 @@ class Blog < ApplicationRecord
 
     validates_presence_of :title, :body
 
+    
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,5 @@ class Portfolio < ApplicationRecord
 
     validates_presence_of :title, :body, :thumb_image
 
+    
 end
