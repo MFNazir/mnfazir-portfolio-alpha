14bd3c7c5819a72d79cd844783514d44edf9d12d
Muhammed Nazir <mnazir9876@gmail.com>
2020-01-03 00:22:40 -0600

    Finally got Sortable to work

--- app/javascript/packs/portfolios.js
+++ app/javascript/packs/portfolios.js
@@ -3,8 +3,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery') 
-require("packs/html.sortable") 
 require("packs/sorts.coffee")  
+require("packs/html.sortable")   
 import "bootstrap";
 import "@fortawesome/fontawesome-free/js/all";
 import "../stylesheets/portfolios"
