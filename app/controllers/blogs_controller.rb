627d5d820cfe5cc93954ab5ec71a751597a1bf90
Muhammed Nazir <mnazir9876@gmail.com>
2020-01-05 20:03:17 -0600

    New title + Blog update

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,7 +10,7 @@ class BlogsController < ApplicationController
     else
       @blogs = Blog.published.recent.page(params[:page]).per(5)
     end
-    @page_title = "My Portfolio Blog"
+    @page_title = "Another Perspective | A Blog"
   end
 
   # GET /blogs/1
